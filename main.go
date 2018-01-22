1968902e0bd7ef52365c360927caae1890f1dabc
Ben Boyter <ben@boyter.org>
2018-01-22 15:04:54 +1100

    Modify slightly

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
-import "path/filepath"
-import "os"
+import (
+	"fmt"
+	vectorspace "github.com/boyter/golangvectorspace"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
-const dirPath = "/home/bboyter/"
+const dirPath = "/home/bboyter/Projects/python-license-checker/"
 
 func main() {
 	// walk all files in directory
 	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			println(info.Name())
+			println(path)
+
+			b, err := ioutil.ReadFile(path) // just pass the file name
+			if err != nil {
+				fmt.Print(err)
+			}
+			str := string(b) // convert content to a 'string'
+
+			var concordance = vectorspace.BuildConcordance(str)
+			println(concordance)
+
 		}
 		return nil
 	})
