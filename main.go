adaa31b77c2e73cf5cd8c0e048e3230b00a033c7
Ben Boyter <ben@boyter.org>
2018-01-24 09:38:00 +1100

    Update to process better

--- main.go
+++ main.go
@@ -8,13 +8,16 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+	"unicode/utf8"
 )
 
 const dirPath = "/home/bboyter/Projects/hyperfine/"
 const pathBlacklist = "/.git/,/.hg/,/.svn/"
 const licenceFiles = "license,copying"
 const extentionBlacklist = "woff,eot,cur,dm,xpm,emz,db,scc,idx,mpp,dot,pspimage,stl,dml,wmf,rvm,resources,tlb,docx,doc,xls,xlsx,ppt,pptx,msg,vsd,chm,fm,book,dgn,blines,cab,lib,obj,jar,pdb,dll,bin,out,elf,so,msi,nupkg,pyc,ttf,woff2,jpg,jpeg,png,gif,bmp,psd,tif,tiff,yuv,ico,xls,xlsx,pdb,pdf,apk,com,exe,bz2,7z,tgz,rar,gz,zip,zipx,tar,rpm,bin,dmg,iso,vcd,mp3,flac,wma,wav,mid,m4a,3gp,flv,mov,mp4,mpg,rm,wmv,avi,m4v,sqlite,class,rlib,ncb,suo,opt,o,os,pch,pbm,pnm,ppm,pyd,pyo,raw,uyv,uyvy,xlsm,swf"
+const confidence = 0.85
 
 type License struct {
 	Keywords    []string `json:"keywords"`
@@ -51,7 +54,7 @@ func loadDatabase(filepath string) []License {
 	}
 
 	for i, v := range database {
-		database[i].Concordance = vectorspace.BuildConcordance(v.Text)
+		database[i].Concordance = vectorspace.BuildConcordance(strings.ToLower(v.Text))
 	}
 
 	return database
@@ -59,11 +62,11 @@ func loadDatabase(filepath string) []License {
 
 func keywordGuessLicense(content string, licenses []License) []LicenseMatch {
 	content = strings.ToLower(content)
-	var matchingLicenses = []LicenseMatch{}
+	matchingLicenses := []LicenseMatch{}
 
 	for _, license := range licenses {
-		var keywordmatch = 0
-		var contains = false
+		keywordmatch := 0
+		contains := false
 
 		for _, keyword := range license.Keywords {
 			contains = strings.Contains(content, keyword)
@@ -73,7 +76,7 @@ func keywordGuessLicense(content string, licenses []License) []LicenseMatch {
 		}
 
 		if keywordmatch > 0 {
-			var percentage = (float64(keywordmatch) / float64(len(license.Keywords))) * 100
+			percentage := (float64(keywordmatch) / float64(len(license.Keywords))) * 100
 			matchingLicenses = append(matchingLicenses, LicenseMatch{Shortname: license.Shortname, Percentage: percentage})
 		}
 	}
@@ -82,25 +85,55 @@ func keywordGuessLicense(content string, licenses []License) []LicenseMatch {
 }
 
 func guessLicense(content string, licenses []License) []LicenseMatch {
-	var matchingLicenses = []LicenseMatch{}
-	var contentConcordance = vectorspace.BuildConcordance(content)
+	matchingLicenses := []LicenseMatch{}
 
 	for _, license := range keywordGuessLicense(content, licenses) {
 
-		var matchingLicense = License{}
+		matchingLicense := License{}
 
 		for _, l := range licenses {
 			if l.Shortname == license.Shortname {
 				matchingLicense = l
+				break
 			}
 		}
 
-		var relation = vectorspace.Relation(matchingLicense.Concordance, contentConcordance)
+		runecontent := []rune(content)
+		trimto := utf8.RuneCountInString(matchingLicense.Text)
 
-		if relation >= 0.85 {
+		if trimto > len(runecontent) {
+			trimto = len(runecontent)
+		}
+
+		contentConcordance := vectorspace.BuildConcordance(string(runecontent[:trimto]))
+		relation := vectorspace.Relation(matchingLicense.Concordance, contentConcordance)
+
+		if relation >= confidence {
 			matchingLicenses = append(matchingLicenses, LicenseMatch{Shortname: license.Shortname, Percentage: relation})
 		}
 	}
+
+	if len(matchingLicenses) == 0 {
+		for _, license := range licenses {
+			runecontent := []rune(content)
+			trimto := utf8.RuneCountInString(license.Text)
+
+			if trimto > len(runecontent) {
+				trimto = len(runecontent)
+			}
+
+			contentConcordance := vectorspace.BuildConcordance(string(runecontent[:trimto]))
+			relation := vectorspace.Relation(license.Concordance, contentConcordance)
+
+			if relation >= confidence {
+				matchingLicenses = append(matchingLicenses, LicenseMatch{Shortname: license.Shortname, Percentage: relation})
+			}
+		}
+	}
+
+	sort.Slice(matchingLicenses, func(i, j int) bool {
+		return matchingLicenses[i].Percentage > matchingLicenses[j].Percentage
+	})
 
 	return matchingLicenses
 }
@@ -114,7 +147,6 @@ func main() {
 	app.Version = "1.0"
 	app.Usage = "Check directory for licenses and list what license(s) a file is under"
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("boom! I say!")
 		return nil
 	}
 
@@ -123,15 +155,16 @@ func main() {
 	// Everything after here needs to be refactored out to a subpackage
 	licenses := loadDatabase("database_keywords.json")
 
-	var extentionBlacklistStrings = strings.Split(extentionBlacklist, ",")
+	extentionBlacklistStrings := strings.Split(extentionBlacklist, ",")
 
 	filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 
-			var run = true
+			run := true
 
 			for _, ext := range extentionBlacklistStrings {
 				if strings.HasSuffix(path, ext) {
+					// Needs to be smarter we should skip reading the contents but it should still be under the license in the root folders
 					run = false
 				}
 			}
@@ -141,19 +174,20 @@ func main() {
 			}
 
 			if run == true {
-				b, err := ioutil.ReadFile(path) // just pass the file name
+				b, err := ioutil.ReadFile(path)
 				if err != nil {
 					fmt.Print(err)
 				}
-				str := string(b) // convert content to a 'string'
+				content := string(b)
 
-				var guesses = guessLicense(str, licenses)
+				licenseGuesses := guessLicense(content, licenses)
 
-				for _, v := range guesses {
-					fmt.Println(">>>>>>>>>>>>>>>>>>>", path, v.Shortname, v.Percentage)
+				licenseString := ""
+				for _, v := range licenseGuesses {
+					licenseString += fmt.Sprintf(" %s (%.2f)", v.Shortname, v.Percentage)
 				}
 
-				fmt.Println(path)
+				fmt.Println(path, licenseString)
 			}
 		}
 
