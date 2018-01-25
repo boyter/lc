a6dd125780f681211a0b6f45afc8876700d35840
Ben Boyter <ben@boyter.org>
2018-01-25 17:13:58 +1100

    Flesh out CSV parser

--- parsers/formatter.go
+++ parsers/formatter.go
@@ -5,11 +5,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func toCSV(fileResults []FileResult) {
-	records := [][]string{
-		{"filename", "directory", "license", "confidence"},
+	records := [][]string{{
+		"filename",
+		"directory",
+		"license",
+		"confidence",
+		"rootlicenses",
+		"md5",
+		"sha1",
+		"sha256",
+		"bytes",
+		"byteshuman"},
 	}
 
 	for _, result := range fileResults {
@@ -19,10 +30,26 @@ func toCSV(fileResults []FileResult) {
 
 		if len(result.LicenseGuesses) != 0 {
 			license = result.LicenseGuesses[0].Shortname
-			confidence = fmt.Sprintf("%.2f", result.LicenseGuesses[0].Percentage*100)
+			confidence = fmt.Sprintf("%.3f", result.LicenseGuesses[0].Percentage*100)
 		}
 
-		records = append(records, []string{result.Filename, result.Directory, license, confidence})
+		rootLicenseString := ""
+		for _, v := range result.LicenseRoots {
+			rootLicenseString += fmt.Sprintf("%s (%.3f%%),", v.Shortname, (v.Percentage * 100))
+		}
+		rootLicenseString = strings.TrimRight(rootLicenseString, ", ")
+
+		records = append(records, []string{
+			result.Filename,
+			result.Directory,
+			license,
+			confidence,
+			rootLicenseString,
+			result.Md5Hash,
+			result.Sha1Hash,
+			result.Sha256Hash,
+			strconv.Itoa(result.Bytes),
+			result.BytesHuman})
 	}
 
 	w := csv.NewWriter(os.Stdout)
@@ -31,4 +58,8 @@ func toCSV(fileResults []FileResult) {
 	if err := w.Error(); err != nil {
 		log.Fatalln("error writing csv:", err)
 	}
+}
+
+func toJSON(fileResults []FileResult) {
+
 }
