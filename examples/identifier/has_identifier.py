e1927b389ffa811d4fa36ad490ed560d4e5736d3
Ben Boyter <ben@boyter.org>
2018-01-31 16:48:32 +1100

    Move things around and add licenses as test

--- examples/identifier/has_identifier.py
+++ examples/identifier/has_identifier.py
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0
-# SPDX-License-Identifier: GPL-3.0+
+
 import cherrypy
 
 class Example(object):
 
+    # SPDX-License-Identifier: GPL-3.0+
     @cherrypy.expose
     def index(self, **params):
         return 'Hello World'
