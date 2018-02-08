d0e3e1ee385c9500286941c5cf4fc3e825fb5311
Ben Boyter <ben@boyter.org>
2018-02-09 08:36:14 +1100

    Addtional tests for formatter logic

--- parsers/formatter_test.go
+++ parsers/formatter_test.go
@@ -1,13 +1,32 @@
 package parsers
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestFormatter(t *testing.T) {
+func TestGeneratePackageVerificationCode(t *testing.T) {
 	result := generatePackageVerificationCode([]FileResult{})
 
 	if result != "da39a3ee5e6b4b0d3255bfef95601890afd80709" {
 		t.Errorf("Expect generatePackageVerificationCode to have da39a3ee5e6b4b0d3255bfef95601890afd80709 but got %q", result)
+	}
+}
+
+func TestGenerateDocumentNamespaceNothingSpecified(t *testing.T) {
+	DocumentNamespace = ""
+	result := generateDocumentNamespace()
+
+	if strings.Contains(result, "http://spdx.org/spdxdocs/") == false {
+		t.Errorf("Expect generateDocumentNamespace to have http://spdx.org/spdxdocs/ but got %q", result)
+	}
+}
+
+func TestGenerateDocumentNamespaceSomethingSpecified(t *testing.T) {
+	DocumentNamespace = "something"
+	result := generateDocumentNamespace()
+
+	if strings.Contains(result, "something") == false {
+		t.Errorf("Expect generateDocumentNamespace to have something but got %q", result)
 	}
 }
