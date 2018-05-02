e5f477f3d8eab77fb6f30fd9c455f35b11333b42
Ben Boyter <ben@boyter.org>
2018-05-02 18:05:21 +1000

    Convert to use goroutines for some of the license guessing

--- parsers/file.go
+++ parsers/file.go
@@ -1,43 +1,52 @@
 package parsers
 
 import (
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
-	"fmt"
 )
 
 func walkDirectoryFast(directory string) {
-	all, _ := ioutil.ReadDir(directory)
+	all, err := ioutil.ReadDir(directory)
+
+	if err != nil {
+		// TODO log error
+		return
+	}
 
 	var directories []string
 	var files []string
 
-	// Work out which directories and files
-	for _, f := range all {
-		if f.IsDir() {
-			directories = append(directories, f.Name())
+	for _, file := range all {
+		if file.IsDir() {
+			directories = append(directories, file.Name())
 		} else {
-			files = append(files, f.Name())
+			files = append(files, file.Name())
 		}
 	}
 
-	// Determine if any of the files might be a possible license deceleration
+	// Determine if any of the files might be a license
 	possibleLicenseFiles := findPossibleLicenseFiles(files)
 
 	// Determine the license for any of the possible files
 	for _, file := range possibleLicenseFiles {
-
 		bytes, err := ioutil.ReadFile(filepath.Join(directory, file))
 
 		if err == nil {
-			guessLicenses := keywordGuessLicenseFast(bytes, loadDatabase())
-			fmt.Println(file, guessLicenses)
+			startTime := makeTimestampMilli()
+			guessLicenses := keywordGuessLicenseFast(bytes, Database)
+
+			if Trace {
+				printTrace(fmt.Sprintf("milliseconds to process file: %s: %d", filepath.Join(directory, file), makeTimestampMilli()-startTime))
+			}
+
+			fmt.Println(filepath.Join(directory, file), guessLicenses)
 
 			//if len(guessLicenses) != 0 {
 			//	identifiedRootLicense = append(identifiedRootLicense, guessLicenses[0])
 			//}
 		} else {
-			// LOG ERROR
+			// TODO log error
 		}
 	}
 
--- parsers/guesserfast.go
+++ parsers/guesserfast.go
@@ -1,65 +1,11 @@
 package parsers
 
 import (
-	"unicode/utf8"
-	"strings"
 	"bytes"
 	"sort"
+	"strings"
+	"sync"
 )
-
-// Parses the supplied file content against the list of licences and
-// returns the matching licences with the shortname and the percentage of match.
-// If fast lookup methods fail it will try deep matching which is slower.
-func guessLicenseFast(content []byte, deepguess bool, licenses []License) []LicenseMatch {
-	var matchingLicenses []LicenseMatch
-
-	for _, license := range keywordGuessLicenseFast(content, licenses) {
-		matchingLicense := License{}
-
-		for _, l := range licenses {
-			if l.LicenseId == license.LicenseId {
-				matchingLicense = l
-				break
-			}
-		}
-
-		trimto := utf8.RuneCountInString(matchingLicense.LicenseText)
-
-		if trimto > len(content) {
-			trimto = len(content)
-		}
-
-		//contentConcordance := vectorspace.BuildConcordance(string(runecontent[:trimto]))
-		//relation := vectorspace.Relation(matchingLicense.Concordance, contentConcordance)
-		//
-		//if relation >= confidence {
-		//	matchingLicenses = append(matchingLicenses, LicenseMatch{LicenseId: license.LicenseId, Percentage: relation})
-		//}
-	}
-
-	//if len(matchingLicenses) == 0 && deepguess == true {
-	//	for _, license := range licenses {
-	//		trimto := utf8.RuneCountInString(license.LicenseText)
-	//
-	//		if trimto > len(content) {
-	//			trimto = len(content)
-	//		}
-	//
-	//		contentConcordance := vectorspace.BuildConcordance(string(content[:trimto]))
-	//		relation := vectorspace.Relation(license.Concordance, contentConcordance)
-	//
-	//		if relation >= confidence {
-	//			matchingLicenses = append(matchingLicenses, LicenseMatch{LicenseId: license.LicenseId, Percentage: relation})
-	//		}
-	//	}
-	//}
-
-	//sort.Slice(matchingLicenses, func(i, j int) bool {
-	//	return matchingLicenses[i].Percentage > matchingLicenses[j].Percentage
-	//})
-
-	return matchingLicenses
-}
 
 // Fast method of checking if supplied content contains a licence using
 // matching keyword ngrams to find if the licence is a match or not
@@ -67,24 +13,36 @@ func guessLicenseFast(content []byte, deepguess bool, licenses []License) []Lice
 func keywordGuessLicenseFast(content []byte, licenses []License) []LicenseMatch {
 	content = cleanTextFast(content)
 
-	var matchingLicenses []LicenseMatch
-	var keywordMatch int
+	var wg sync.WaitGroup
+	output := make(chan LicenseMatch, 10)
 
 	for _, license := range licenses {
-		keywordMatch = 0
+		wg.Add(1)
+		go func(license License) {
+			keywordMatch := 0
 
-		for _, keyword := range license.Keywords {
-			if bytes.Contains(content, []byte(strings.ToLower(keyword))) {
-				keywordMatch++
+			for _, keyword := range license.Keywords {
+				if bytes.Contains(content, []byte(strings.ToLower(keyword))) {
+					keywordMatch++
+				}
 			}
-		}
 
-		if keywordMatch > 0 {
-			percentage := (float64(keywordMatch) / float64(len(license.Keywords))) * 100
-			if percentage > 70 {
-				matchingLicenses = append(matchingLicenses, LicenseMatch{LicenseId: license.LicenseId, Percentage: percentage})
+			if keywordMatch > 0 {
+				percentage := (float64(keywordMatch) / float64(len(license.Keywords))) * 100
+				if percentage > 70 {
+					output <- LicenseMatch{LicenseId: license.LicenseId, Percentage: percentage}
+				}
 			}
-		}
+			wg.Done()
+		}(license)
+	}
+
+	wg.Wait()
+	close(output)
+
+	var matchingLicenses []LicenseMatch
+	for license := range output {
+		matchingLicenses = append(matchingLicenses, license)
 	}
 
 	sort.Slice(matchingLicenses, func(i, j int) bool {
@@ -93,14 +51,10 @@ func keywordGuessLicenseFast(content []byte, licenses []License) []LicenseMatch 
 
 	if len(matchingLicenses) > 2 && ((matchingLicenses[0].LicenseId == "JSON" && matchingLicenses[1].LicenseId == "MIT") ||
 		(matchingLicenses[0].LicenseId == "MIT" && matchingLicenses[1].LicenseId == "JSON")) {
-		if bytes.Contains(bytes.ToLower(content), []byte("not evil")) {
-			// Its JSON
-			matchingLicenses = []LicenseMatch{}
-			matchingLicenses = append(matchingLicenses, LicenseMatch{LicenseId: "JSON", Percentage: 1})
+		if bytes.Contains(content, []byte("not evil")) {
+			matchingLicenses = []LicenseMatch{{LicenseId: "JSON", Percentage: 1}}
 		} else {
-			// Its MIT
-			matchingLicenses = []LicenseMatch{}
-			matchingLicenses = append(matchingLicenses, LicenseMatch{LicenseId: "MIT", Percentage: 1})
+			matchingLicenses = []LicenseMatch{{LicenseId: "MIT", Percentage: 1}}
 		}
 	}
 
