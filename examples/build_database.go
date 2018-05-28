24978d02881ee83af026d6726c00ef4f6170cc81
Ben Boyter <ben@boyter.org>
2018-05-28 18:11:03 +1000

    Improve build database script to be faster not use all the RAM

--- examples/build_database.go
+++ examples/build_database.go
@@ -9,7 +9,6 @@ import (
 	"regexp"
 	"strings"
 	"sync"
-	"time"
 )
 
 type License struct {
@@ -17,7 +16,7 @@ type License struct {
 	StandardLicenseTemplate string `json:"standardLicenseTemplate"`
 	Name                    string `json:"name"`
 	LicenseId               string `json:"licenseId"`
-	Ngrams                  [][]string
+	Ngrams                  []string
 }
 
 type LicenseOutput struct {
@@ -28,7 +27,6 @@ type LicenseOutput struct {
 	Keywords                []string `json:"keywords"`
 }
 
-var spdxLicenceRegex = regexp.MustCompile(`SPDX-License-Identifier:\s+(.*)[ |\n|\r\n]*?`)
 var alphaNumericRegex = regexp.MustCompile("[^a-zA-Z0-9 ]")
 var multipleSpacesRegex = regexp.MustCompile("\\s+")
 
@@ -41,14 +39,13 @@ func cleanText(content string) string {
 	return content
 }
 
-func findNgrams(list []string, size int) [][]string {
-	var ngrams [][]string
+func findNgrams(list []string, size int) []string {
+	var ngrams []string
 
 	for i := 0; i < len(list); i++ {
-
 		if i+size < len(list)+1 {
 			ngram := list[i : i+size]
-			ngrams = append(ngrams, ngram)
+			ngrams = append(ngrams, strings.Join(ngram, " "))
 		}
 	}
 
@@ -67,7 +64,7 @@ func main() {
 
 			var license License
 			json.Unmarshal(bytes, &license)
-			license.Ngrams = [][]string{}
+			license.Ngrams = []string{}
 
 			licenses = append(licenses, license)
 		}
@@ -83,7 +80,8 @@ func main() {
 		}
 	}
 
-	//var licensesOutput []LicenseOutput
+
+	outputChan := make(chan LicenseOutput, 2000)
 
 	var wg sync.WaitGroup
 	// For each licence, check each ngram and see if it is unique
@@ -95,48 +93,53 @@ func main() {
 			// for each licence that isn't this one
 			// get all the ngrams and put it into a hash
 			// then look each of our ngrams and check if it is contained
-			contains := map[string]int{}
+			contains := map[*string]int{}
 			for _, lic := range licenses {
 				if lic.LicenseId != license.LicenseId {
 					for _, ngram := range lic.Ngrams {
-						contains[strings.Join(ngram, "")] = 1
+						contains[&ngram] = 1
 					}
 				}
 			}
 
 			var uniqueNgrams []string
 			for _, ngram := range license.Ngrams {
-				_, ok := contains[strings.Join(ngram, "")]
+				_, ok := contains[&ngram]
 
 				if !ok {
-					uniqueNgrams = append(uniqueNgrams, strings.Join(ngram, " "))
+					uniqueNgrams = append(uniqueNgrams, ngram)
+				}
+
+				if len(uniqueNgrams) >= 50 {
+					break
 				}
 			}
 
 			fmt.Println(license.LicenseId, len(license.Ngrams), "Unique Ngrams", len(uniqueNgrams))
+
+			outputChan <- LicenseOutput{
+				LicenseId: license.LicenseId,
+				Name: license.Name,
+				LicenseText: license.LicenseText,
+				StandardLicenseTemplate: license.StandardLicenseTemplate,
+				Keywords: uniqueNgrams,
+			}
+
 			wg.Done()
 		}(i)
-
-		// This is such an ugly way to control concurrency
-		if i%4 == 0 { // This uses about 8 GB of RAM currently.
-			time.Sleep(time.Second * 5) // Gotta wait for the goroutines to actually kick off first
-			fmt.Println("Waiting for results...")
-			wg.Wait()
-		}
 	}
 
 	wg.Wait()
+	close(outputChan)
+
+	var outputLicenses []LicenseOutput
+	for lic := range outputChan {
+		outputLicenses = append(outputLicenses, lic)
+	}
+
 	out, _ := os.Create("database_keywords.json")
 
-	data, _ := json.Marshal(licenses)
+	data, _ := json.Marshal(outputLicenses)
 	out.Write(data)
 	out.Close()
-
-	//licenses = [{
-	//	'licenseText': x['licenseText'],
-	//	'name': x['name'],
-	//	'licenseId': x['licenseId'],
-	//	'keywords': x['keywords'][:50]
-	//} for x in licenses]
-
 }
