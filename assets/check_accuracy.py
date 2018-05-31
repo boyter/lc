491108e5dd6f12da9988ec1fcacc151d883994ba
Ben Boyter <ben@boyter.org>
2018-05-31 17:39:28 +1000

    Update to have more tools

--- assets/check_accuracy.py
+++ assets/check_accuracy.py
@@ -48,8 +48,21 @@ def check_identify_license(targets):
     return correct
 
 
+def check_askalono(targets):
+    correct = 0
+
+    for target in targets:
+        data = commands.getstatusoutput('askalono id %s/LICENSE.txt' % (target))
+        license = target.split('/')[2]
+
+        if license == data[1].split('\n')[0].split(' ')[1]:
+            correct += 1
+
+    return correct
+
+
 if __name__ == '__main__':
-    targets = ['./accuracy/' + x + '/' for x in os.listdir('./accuracy')][:50]
+    targets = ['./accuracy/' + x + '/' for x in os.listdir('./accuracy')][:1]
 
     print 'count::%s' % (len(targets))
     print 'checking::lc'
@@ -68,26 +81,7 @@ if __name__ == '__main__':
     correct = check_identify_license(targets)
     print 'correct:%s::%s percent::time:%s' % (correct, float(correct) / float(len(targets)) * 100, time.time() - start)
 
-
-
-# Sample
-# for target in targets:
-#     data = commands.getstatusoutput('lc -f csv %s' % (target))
-#     split = data[1].split(',')
-
-#     if split[11] == split[13]:
-#         correct += 1
-#     # //11
-#     # //13
-#     # glob = re.findall(r"accuracy/(.*?)/LICENSE.txt(.*)", data[1])
-#     # if len(glob) != 1:
-#     #     print data[1]
-#     #     print glob
-#     # if glob[0][0] == glob[0][1].strip():
-#     #     correct += 1
-
-
-# print 'correct::%s::%s percent' % (correct, float(correct) / float(len(targets)) * 100)
-
-
-
+    start = time.time()
+    print 'checking::askalono'
+    correct = check_askalono(targets)
+    print 'correct:%s::%s percent::time:%s' % (correct, float(correct) / float(len(targets)) * 100, time.time() - start)
